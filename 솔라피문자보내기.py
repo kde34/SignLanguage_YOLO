48f52b7e631d7c6876311a17b7f6b9b4a0f1cab2
KimHajin4 <hajin0988@naver.com>
2026-05-08 14:18:10 +0900

    Update 솔라피문자보내기.py

--- 솔라피문자보내기.py
+++ 솔라피문자보내기.py
@@ -7,10 +7,10 @@ import hashlib
 def send_solapi_sms(to_number, translated_word):
     """솔라피를 이용해 번역된 수어를 문자로 전송하는 함수"""
     
-    # ⚠️ 여기에 솔라피 홈페이지에서 발급받은 정보를 쏙쏙 넣어주세요!
+    # 솔라피 홈페이지에서 발급받은 정보 입력
     API_KEY = "--"
     API_SECRET = "--"
-    FROM_NUMBER = "--" # 예: 01012345678 (하이픈 빼고 숫자만!)
+    FROM_NUMBER = "--" # 예: 01012345678 (하이픈 빼고 숫자만)
     
     # 솔라피 공식 암호화(HMAC) 인증 과정
     date = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
@@ -19,7 +19,7 @@ def send_solapi_sms(to_number, translated_word):
     signature = hmac.new(API_SECRET.encode(), data.encode(), hashlib.sha256).hexdigest()
     auth_header = f'HMAC-SHA256 apiKey={API_KEY}, date={date}, salt={salt}, signature={signature}'
     
-    # 💌 스마트폰으로 날아갈 문자 내용 구성!
+    # 스마트폰으로 날아갈 문자 내용 구성
     text_message = f"[9조 실시간 수어 번역]\n방금 인식된 수어는 '{translated_word}' 입니다!"
     
     try:
